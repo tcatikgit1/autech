06132eb25af78d6f15a95d625f6043bd0fad3839
Alejandro <yoquiale@gmail.com>
2025-03-14 12:25:06 +0000

    AD:Configuración del comportamiento del banner

--- resources/views/layouts/sections/content/banner.blade.php
+++ resources/views/layouts/sections/content/banner.blade.php
@@ -1,6 +1,5 @@
-
 <div class="banner">
-    <img src="{{'assets/img/banner.png'}}" alt="Banner de bienvenida" style="width: 100%; height: auto;">
+    <img src="{{'assets/img/banner.png'}}" alt="Banner de bienvenida">
     <h1>Servicios integrales</h1>
     <h4>con soluciones efectivas</h4>
 </div>
