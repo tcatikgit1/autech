d441a9dfffbad4aaa01434697337b0aacd1071ee
Alejandro <yoquiale@gmail.com>
2025-03-17 15:05:00 +0000

    AD: Añadido del logo "Nuestros servicios"

--- resources/views/layouts/sections/content/carrousel.blade.php
+++ resources/views/layouts/sections/content/carrousel.blade.php
@@ -1,5 +1,18 @@
 <section class="game-section">
-  <h2 class="line-title">Nuestros servicios</h2>
+    <h2 class="line-title">
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"     stroke="currentColor"      stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-packages">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M7 16.5l-5 -3l5 -3l5 3v5.5l-5 3z" />
+            <path d="M2 13.5v5.5l5 3" />
+            <path d="M7 16.545l5 -3.03" />
+            <path d="M17 16.5l-5 -3l5 -3l5 3v5.5l-5 3z" />
+            <path d="M12 19l5 3" />
+            <path d="M17 16.5l5 -3" />
+            <path d="M12 13.5v-5.5l-5 -3l5 -3l5 3v5.5" />
+            <path d="M7 5.03v5.455" />
+            <path d="M12 8l5 -3" />
+        </svg>
+  Nuestros servicios</h2>
   <div class="owl-carousel custom-carousel owl-theme">
     <div class="item active" style="background-image: url(assets/img/caroussel/img1.png);">
       <div class="item-desc">
