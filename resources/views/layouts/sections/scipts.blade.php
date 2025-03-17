e91ce2025cc5ced4e0ff57c9789875b47b5c2976
Alejandro <yoquiale@gmail.com>
2025-03-17 14:00:29 +0000

    AD: Implementación del nuevo Carrousel

--- resources/views/layouts/sections/scipts.blade.php
+++ resources/views/layouts/sections/scipts.blade.php
@@ -28,5 +28,5 @@
 
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
