35eddb01b5f777eeec8ca01351cb5dfcb0b8ecab
tcatik-dev16 <desarrollador17@tcatik.com>
2025-03-17 09:11:43 +0000

    JB arreglando estilos

--- resources/views/layouts/sections/content/partners.blade.php
+++ resources/views/layouts/sections/content/partners.blade.php
@@ -1,13 +1,13 @@
 <div class="partners">
     <div class="partners-track" id="partnersTrack">
-        <img src="{{'assets/img/partners/partner.png'}}" alt="Imagen 1">
-        <img src="{{'assets/img/partners/partner2.png'}}" alt="Imagen 2">
-        <img src="{{'assets/img/partners/partner3.png'}}" alt="Imagen 3">
-        <img src="{{'assets/img/partners/partner4.png'}}" alt="Imagen 4">
-        <img src="{{'assets/img/partners/partner.png'}}" alt="Imagen 5">
-        <img src="{{'assets/img/partners/partner2.png'}}" alt="Imagen 6">
-        <img src="{{'assets/img/partners/partner3.png'}}" alt="Imagen 7">
-        <img src="{{'assets/img/partners/partner4.png'}}" alt="Imagen 8">
+        <img src="{{'assets/img/partners/partner.png'}}" alt="partner 1">
+        <img src="{{'assets/img/partners/partner2.png'}}" alt="partner 2">
+        <img src="{{'assets/img/partners/partner3.png'}}" alt="partner 3">
+        <img src="{{'assets/img/partners/partner4.png'}}" alt="partner 4">
+        <img src="{{'assets/img/partners/partner.png'}}" alt="partner 5">
+        <img src="{{'assets/img/partners/partner2.png'}}" alt="partner 6">
+        <img src="{{'assets/img/partners/partner3.png'}}" alt="partner 7">
+        <img src="{{'assets/img/partners/partner4.png'}}" alt="partner 8">
     </div>
 </div>
 
