7b1568a558580e1dcd1a6a101392de174190d2fa
Alejandro <yoquiale@gmail.com>
2025-03-17 14:05:41 +0000

    AD: Limpieza del código relacionado con el antiguo carrousel

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -10,15 +10,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    {{-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> --}}
-
-
-    <!-- UIkit CSS -->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.23.1/dist/css/uikit.min.css" />
-
-    <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.23.1/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.23.1/dist/js/uikit-icons.min.js"></script> --}}
 
     {{-- Logo --}}
     <link rel="icon" type="image/x-icon" href="{{'assets/img/Logo.png'}}" alt="logo">
