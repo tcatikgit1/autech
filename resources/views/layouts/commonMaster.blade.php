cbda40a5d6f75f65a342241757862eaf4fd872e5
tcatik-dev16 <desarrollador17@tcatik.com>
2025-03-18 09:59:43 +0000

    JB: trabajando estilos

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -15,8 +15,10 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Oswald:wght@200..700&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-            rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     {{-- Logo --}}
     <link rel="icon" type="image/x-icon" href="{{'assets/img/Logo.png'}}" alt="logo">
@@ -26,7 +28,7 @@
 
 </head>
 
-<body >
+<body>
 
     @yield('layoutContent')
 
--- resources/views/layouts/sections/content/carrousel.blade.php
+++ resources/views/layouts/sections/content/carrousel.blade.php
@@ -1,4 +1,4 @@
-<section class="game-section">
+<section class="game-section" id="servicios">
     <h2 class="line-title">
         <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"     stroke="currentColor"      stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-packages">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
