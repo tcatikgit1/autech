adb1bb4613929aaa92db7ab60c85483a43e350e1
Alejandro <yoquiale@gmail.com>
2025-03-18 09:21:47 +0000

    AD: Banner responsive

--- resources/views/layouts/sections/content/banner.blade.php
+++ resources/views/layouts/sections/content/banner.blade.php
@@ -2,8 +2,12 @@
     <!-- Contenedor de las partículas -->
     <div id="particles-js"></div>
 
+    <!-- Imagen de fondo -->
     <img src="{{'assets/img/banner.png'}}" alt="Banner de bienvenida">
 
-    <h1>Servicios integrales</h1>
-    <h4>con soluciones efectivas</h4>
+    <!-- Contenedor del texto -->
+    <div class="text-container">
+        <h1>Servicios integrales</h1>
+        <h4>con soluciones efectivas</h4>
+    </div>
 </div>
